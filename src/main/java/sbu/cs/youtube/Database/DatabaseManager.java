ec3b44edcb5b97c75089dae239d608dbde40a093
Ali Taherzadeh <Ali.Thz83@gmail.com>
2024-06-05 22:45:18 +0330

    database connection variables added

---
+++ src/main/java/sbu/cs/youtube/Database/DatabaseManager.java
@@ -0,0 +1,26 @@
+package sbu.cs.youtube.Database;
+
+import sbu.cs.youtube.Shared.POJO.Channel;
+import sbu.cs.youtube.Shared.POJO.Notification;
+import sbu.cs.youtube.Shared.POJO.Subscription;
+import sbu.cs.youtube.Shared.POJO.User;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class DatabaseManager {
+
+    //region [ - Fields - ]
+    private final String URL = "jdbc:postgresql://localhost:5432/Youtube-Development";
+    private final String USER = "postgres";
+    private final String PASSWORD = "musketeers";
+    //endregion
+
+    //region [ - Methods - ]
+
+
+
+    //endregion
+}
