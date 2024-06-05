b8d838fc05ff10f678953e83cb35a03e0ca84563
SrgtSajjad <msajjad.za135@gmail.com>
2024-06-05 21:48:35 +0330

    dark mode nav-bar completed

--- src/main/java/sbu/cs/youtube/Client/Controller/HomePageController.java
+++ src/main/java/sbu/cs/youtube/Client/Controller/HomePageController.java
@@ -1,4 +1,13 @@
 package sbu.cs.youtube.Client.Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class HomePageController {
+
 }
