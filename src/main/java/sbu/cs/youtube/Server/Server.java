163e8db0022b1112a51acef1d0494698954f1edf
Ali Taherzadeh <Ali.Thz83@gmail.com>
2024-06-08 14:50:18 +0330

    Server class implemented

---
+++ src/main/java/sbu/cs/youtube/Server/Server.java
@@ -0,0 +1,28 @@
+package sbu.cs.youtube.Server;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(2345);
+            System.out.println("SERVER HAS STARTED LISTENING ON PORT '2345'");
+
+            while (true) {
+                //Waiting for clients to connect
+                Socket socket = serverSocket.accept();
+
+                //Giving service to each client
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(String.valueOf(clientHandler));
+                thread.start();
+            }
+        } catch (IOException ioe) {
+            System.out.println("!!Exception : " + ioe.getMessage());
+        }
+    }
+}
