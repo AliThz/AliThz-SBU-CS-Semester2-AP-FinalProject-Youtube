7d38729afd140dac65d1d7287e9732374b49e2d9
SrgtSajjad <msajjad.za135@gmail.com>
2024-06-28 20:11:40 +0330

    circularized avatar

--- src/main/java/sbu/cs/youtube/Client/Controller/LayoutController.java
+++ src/main/java/sbu/cs/youtube/Client/Controller/LayoutController.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.SVGPath;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -65,7 +66,7 @@ public class LayoutController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         if (YouTubeApplication.user == null) {
-            isSignedIn = false;
+            isSignedIn = true;
         } else {
             isSignedIn = true;
         }
@@ -379,8 +380,6 @@ public class LayoutController implements Initializable {
 
     protected void setRightNavBarHBox() {
         if (!isSignedIn) {
-
-
             Button signInBtn = new Button();
             SVGPath signInSvg = new SVGPath();
             signInSvg.setContent("M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 1c4.96 0 9 4.04 9 9 0 1.42-.34 2.76-.93 3.96-1.53-1.72-3.98-2.89-7.38-3.03A3.996 3.996 0 0016 9c0-2.21-1.79-4-4-4S8 6.79 8 9c0 1.97 1.43 3.6 3.31 3.93-3.4.14-5.85 1.31-7.38 3.03C3.34 14.76 3 13.42 3 12c0-4.96 4.04-9 9-9zM9 9c0-1.65 1.35-3 3-3s3 1.35 3 3-1.35 3-3 3-3-1.35-3-3zm3 12c-3.16 0-5.94-1.64-7.55-4.12C6.01 14.93 8.61 13.9 12 13.9c3.39 0 5.99 1.03 7.55 2.98C17.94 19.36 15.16 21 12 21z");
@@ -403,6 +402,7 @@ public class LayoutController implements Initializable {
             hbxRightNavItem.getChildren().clear();
             hbxRightNavItem.getChildren().add(signInBtn);
         } else {
+
             // create buttons
             Button createBtn = new Button();
             createBtn.getStyleClass().add("loggedIn-btn");
@@ -422,8 +422,10 @@ public class LayoutController implements Initializable {
             notificationsSvg.setContent("M10 20h4c0 1.1-.9 2-2 2s-2-.9-2-2zm10-2.65V19H4v-1.65l2-1.88v-5.15C6 7.4 7.56 5.1 10 4.34v-.38c0-1.42 1.49-2.5 2.99-1.76.65.32 1.01 1.03 1.01 1.76v.39c2.44.75 4 3.06 4 5.98v5.15l2 1.87zm-1 .42-2-1.88v-5.47c0-2.47-1.19-4.36-3.13-5.1-1.26-.53-2.64-.5-3.84.03C8.15 6.11 7 7.99 7 10.42v5.47l-2 1.88V18h14v-.23z");
             notificationsSvg.getStyleClass().add("notifications-svg");
 
+            Circle clip = new Circle(12, 12, 12);
             Label accountImg = new Label();
             accountImg.getStyleClass().add("account-img");
+            accountImg.setClip(clip);
 
             createBtn.setGraphic(createSvg);
             notificationsBtn.setGraphic(notificationsSvg);
