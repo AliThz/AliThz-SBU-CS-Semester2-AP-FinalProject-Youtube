4e7d0b0a37a6f2c07e8f9d1b0e68c8acf07b5a41
SrgtSajjad <msajjad.za135@gmail.com>
2024-07-09 13:59:38 +0330

    playlist light mode implemented

--- src/main/java/sbu/cs/youtube/Client/Controller/PlaylistSectionController.java
+++ src/main/java/sbu/cs/youtube/Client/Controller/PlaylistSectionController.java
@@ -1,5 +1,7 @@
 package sbu.cs.youtube.Client.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,9 +12,11 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import sbu.cs.youtube.Shared.POJO.Playlist;
+import sbu.cs.youtube.YouTubeApplication;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class PlaylistSectionController implements Initializable {
@@ -28,6 +32,10 @@ public class PlaylistSectionController implements Initializable {
     //region [ - initialize(URL location, ResourceBundle resources) - ]
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        mainPane.getStylesheets().clear();
+        mainPane.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/Styles/" + YouTubeApplication.theme + "/playlist-page.css")).toExternalForm());
+
         FXMLLoader layoutLoader = new FXMLLoader(getClass().getResource("/sbu/cs/youtube/layout.fxml"));
         Parent layout;
         try {
@@ -54,11 +62,23 @@ public class PlaylistSectionController implements Initializable {
             throw new RuntimeException(e);
         }
         layoutController.hbxContent.getChildren().add(playlistPage);
+        PlaylistPageController playlistPageController = PlaylistPageLoader.getController();
+        playlistPageController.setParentController(layoutController);
         HBox.setHgrow(playlistPage, Priority.ALWAYS);
 //        playlistPage.prefWidthProperty().bind(layoutController.hbxContent.prefWidthProperty());
 //        playlistPage.prefHeightProperty().bind(layoutController.hbxContent.prefHeightProperty());
         playlistPage.prefWidthProperty().bind(layoutController.hbxContent.widthProperty().subtract(layoutController.vbxSideBar.widthProperty().add(10)));
         playlistPage.prefHeightProperty().bind(layoutController.hbxContent.heightProperty());
+
+        EventHandler<ActionEvent> existingHandler = layoutController.btnMode.getOnAction();
+
+        layoutController.btnMode.setOnAction(event -> {
+            if (existingHandler != null) {
+                existingHandler.handle(event);
+            }
+            mainPane.getStylesheets().clear();
+            mainPane.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/Styles/" + YouTubeApplication.theme + "/playlist-page.css")).toExternalForm());
+        });
     }
     //endregion
 
